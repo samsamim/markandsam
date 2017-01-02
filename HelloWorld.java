6216374ee95009cd35f216dfbb62ea042fc15937
samsamim <samsamimm@gmail.com>
2017-01-01 17:12:22 -0800

    dezz nut love

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,5 +2,6 @@ public class HelloWorld {
    public static void main(String[] args) {
       // Prints "Hello, World" in the terminal window.
       System.out.println("Dese nuts");
+      System.out.println("I like dezz nut too");
    }
 }
