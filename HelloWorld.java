b7dbc2b31485d6731357cdf999c7b1dfe460cac2
samsamim <samsamimm@gmail.com>
2017-01-01 17:31:10 -0800

    nuts are good

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,7 @@
 public class HelloWorld {
    public static void main(String[] args) {
       // Prints "Hello, World" in the terminal window.
-      System.out.println("more nuts");
+      System.out.println("more nuts is good");
       System.out.println("Dese nuts");
       System.out.println("I like dezz nut too");
    }
